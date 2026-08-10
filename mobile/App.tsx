9eaa2fe300e35f783d05af86e9d6b6dd5b94f4b9
Sike-tech <sarang.rm.vishnu@gmail.com>
2026-08-10 23:34:26 +0530

    Remove asset preloading, restore original App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,26 +5,10 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import * as Asset from 'expo-asset';
 import { ProfileProvider } from './src/hooks/useProfile';
 import AppNavigator from './src/navigation/AppNavigator';
 
 SplashScreen.preventAutoHideAsync();
-
-const ASSETS = [
-  require('./assets/home-page.png'),
-  require('./assets/question-page.png'),
-  require('./assets/royal-elephant-game-ui-5x5.png'),
-  require('./assets/royal-elephant-game-ui-6x6.png'),
-  require('./assets/royal-elephant-game-ui-7x7.png'),
-  require('./assets/royal-elephant-game-ui-8x8.png'),
-  require('./assets/royal-elephant-levels.png'),
-  require('./assets/victory-dialog-0-star.png'),
-  require('./assets/victory-dialog-1-star.png'),
-  require('./assets/victory-dialog-2-star.png'),
-  require('./assets/victory-dialog-3-star.png'),
-  require('./assets/Ashok-Chakra.png'),
-];
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -32,10 +16,6 @@ export default function App() {
     'Cinzel': require('./assets/Cinzel Family/Cinzel/Cinzel-Regular.ttf'),
     'Cinzel-Black': require('./assets/Cinzel Family/Cinzel/Cinzel-Black.ttf'),
   });
-
-  useEffect(() => {
-    Asset.loadAsync(ASSETS).catch(() => {});
-  }, []);
 
   useEffect(() => {
     if (fontsLoaded) {
